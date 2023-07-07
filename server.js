005ffbe00d85e7d8c69d14dd685617217b1a4287
Essence1987 <hwmelander@gmail.com>
2023-07-06 18:43:56 -0600

    fixed port issue so it should now work with Heroku

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API server is running on port ${port}`);
 });
