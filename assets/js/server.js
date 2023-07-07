637f3690689615d5773c9bbfcf2f67b6a885188c
Essence1987 <hwmelander@gmail.com>
2023-07-06 20:52:52 -0600

    added a delete path for the API

--- assets/js/server.js
+++ assets/js/server.js
@@ -58,6 +58,41 @@ app.post('/api/notes', (req, res) => {
   });
 });
 
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile(dbFilePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ error: 'Unable to read notes from the database.' });
+      return;
+    }
+
+    const notes = JSON.parse(data);
+    const noteId = req.params.id;
+
+    // Find the index of the note with the given ID
+    const noteIndex = notes.findIndex((note) => note.id === noteId);
+
+    if (noteIndex === -1) {
+      // Note with the given ID was not found
+      res.status(404).json({ error: 'Note not found.' });
+      return;
+    }
+
+    // Remove the note from the array
+    notes.splice(noteIndex, 1);
+
+    fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Unable to delete the note.' });
+        return;
+      }
+
+      res.json({ message: 'Note deleted successfully.' });
+    });
+  });
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`API server is running on port ${port}`);
